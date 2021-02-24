8ec60b646f60a09673af5bc3e7c9d9a0dceb6fcb
abhinavkumar18 <78842394+abhinavkumar18@users.noreply.github.com>
2021-02-24 16:28:16 +0530

    Update Advice.java

--- Advice.java
+++ Advice.java
@@ -4,82 +4,46 @@
  * and open the template in the editor.
  */
 package project1;
-import java.io.*;
-import java.util.*;
+
 /**
  *
- * @author Akash
+ * @author Abhinav
  */
 public class Advice {
-    public String filepath="C:\\Users\\Akash\\Desktop\\CSV files\\Advice.csv";
+public String filepath="C:\\Users\\DELL\\OneDrive\\Desktop\\CSV files";
     
-    public String description;
+    public String patientid;
+    public String patientname;
     public String datetime;
+    public String description;
     
-    public Advice(String desc,String date){
-        this.description=desc;
-        this.datetime=date;
-    }
     
-    public void sendAdvice(){
-        FileWriter filewriter=null;
-        try{
-            filewriter= new FileWriter(filepath,true);
-            filewriter.append(this.description);
-            filewriter.append(",");
-            filewriter.append(this.datetime);
-            filewriter.append("\n");
-            
-        }catch(Exception ex){
-            ex.printStackTrace();
-            
-        }
-        finally{
-            try{
-                filewriter.flush();
-                filewriter.close();
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
+     public String getpatientid(){
+        return patientid;
     }
-    
-    public ArrayList<Advice> getAdvice(){
-        BufferedReader reader=null;
-        ArrayList<Advice> Adarr=new ArrayList<Advice>();
-        try{
-            String line="";
-            reader=new BufferedReader(new FileReader(filepath));
-            reader.readLine();
-            
-            while((line=reader.readLine())!=null){
-                String[] fields= line.split(",");
-                
-                if(fields.length>0){
-                    Advice a= new Advice();
-                    a.description=fields[0];
-                    a.datetime=fields[1];
-                    Adarr.add(a);
-                }
-            }
-        }catch(Exception ex){
-            ex.printStackTrace();
-          
-        }
-        finally{
-            try{
-                reader.close();
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-        for(Advice ad:Adarr){
-            System.out.printf("Description=%s,Date/time=%s\n",ad.description,ad.datetime);
-            System.out.println();
-        }
-       return Adarr;
+    public String getpatientname(){
+        return patientname;
     }
-    Advice(){
+    public String getdatetime(){
+        return datetime;
+    }
+    public String getdescription(){
+        return description;
+    }
+public Advice(String patientid,String patientname,String datetime,String description){
+        this.patientid=patientid;
+        this.patientname=patientname;
+        this.datetime=datetime;
+        this.description=description;
         
     }
+    
+    public String toString(){
+        return "Patient Id:"+getpatientid()+"Patient Name:"+getpatientname()+
+                "Date Time:"+getdatetime()+"Description:"+getdescription();
+        
+    }
+    Advice(){
 }
+}
+    
